06d5e0b15e97f53447816c4ad6d5d5042098e2ee
billykerz <34313952+billykerz@users.noreply.github.com>
2024-12-19 18:17:40 -0500

    Add stored-objects.js with initial TEST_RECTANGLE object

--- objects/stored-objects.js
+++ objects/stored-objects.js
@@ -1,12 +1,4 @@
-window.STORED_OBJECTS = {
-  TEST_RECTANGLE: {
-    "type": "RECTANGLE",
-    "name": "Test Rectangle",
-    "width": 100,
-    "height": 100,
-    "fills": [{
-      "type": "SOLID",
-      "color": {"r": 1, "g": 0, "b": 0}
-    }]
-  }
-};
+{
+    "encoding": "base64",
+    "content": "d2luZG93LlNUT1JFRF9PQkpFQ1RTID0gewogIFRFU1RfUkVDVEFOR0xFOiB7CiAgICAidHlwZSI6ICJSRUNUQU5HTEUiLAogICAgIm5hbWUiOiAiVGVzdCBSZWN0YW5nbGUiLAogICAgIndpZHRoIjogMTAwLAogICAgImhlaWdodCI6IDEwMCwKICAgICJmaWxscyI6IFt7CiAgICAgICJ0eXBlIjogIlNPTElEIiwKICAgICAgImNvbG9yIjogeyJyIjogMSwgImciOiAwLCAiYiI6IDB9CiAgICB9XQogIH0KfTs="
+}
