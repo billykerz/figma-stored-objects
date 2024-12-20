f03ec8bcae87466187c4e1f13f7c929472481a2b
William Kersnowski <billy@dobkingdesigngroup.com>
2024-12-20 14:31:14 -0500

    Update TEST_RECTANGLE to be green and 400x400

--- objects/stored-objects.js
+++ objects/stored-objects.js
@@ -2,14 +2,14 @@ window.STORED_OBJECTS = {
   TEST_RECTANGLE: {
     "type": "RECTANGLE",
     "name": "Test Rectangle (GitHub)",
-    "width": 200,
-    "height": 200,
+    "width": 400,
+    "height": 400,
     "fills": [{
       "type": "SOLID",
       "color": {
         "r": 0,
-        "g": 0,
-        "b": 1
+        "g": 1,
+        "b": 0
       }
     }]
   }
