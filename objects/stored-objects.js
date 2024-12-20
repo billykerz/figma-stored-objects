71522b9dc7c1f5fa688b9bc68b5220eae7595345
William Kersnowski <billy@dobkingdesigngroup.com>
2024-12-20 13:27:56 -0500

    Format stored objects file with proper line breaks

--- objects/stored-objects.js
+++ objects/stored-objects.js
@@ -1,1 +1,16 @@
-window.STORED_OBJECTS = { TEST_RECTANGLE: { "type": "RECTANGLE", "name": "Test Rectangle (GitHub)", "width": 200, "height": 200, "fills": [{ "type": "SOLID", "color": {"r": 0, "g": 0, "b": 1} }] } };
+window.STORED_OBJECTS = {
+  TEST_RECTANGLE: {
+    "type": "RECTANGLE",
+    "name": "Test Rectangle (GitHub)",
+    "width": 200,
+    "height": 200,
+    "fills": [{
+      "type": "SOLID",
+      "color": {
+        "r": 0,
+        "g": 0,
+        "b": 1
+      }
+    }]
+  }
+};
