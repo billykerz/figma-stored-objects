ecdf473b4630ca753a135f11f4f57767cd4cccb2
William Kersnowski <billy@dobkingdesigngroup.com>
2024-12-20 10:45:49 -0500

    Update TEST_RECTANGLE to be blue and larger

--- objects/stored-objects.js
+++ objects/stored-objects.js
@@ -1,12 +1,1 @@
-window.STORED_OBJECTS = {
-  TEST_RECTANGLE: {
-    "type": "RECTANGLE",
-    "name": "Test Rectangle",
-    "width": 100,
-    "height": 100,
-    "fills": [{
-      "type": "SOLID",
-      "color": {"r": 1, "g": 0, "b": 0}
-    }]
-  }
-};
+window.STORED_OBJECTS = { TEST_RECTANGLE: { "type": "RECTANGLE", "name": "Test Rectangle (GitHub)", "width": 200, "height": 200, "fills": [{ "type": "SOLID", "color": {"r": 0, "g": 0, "b": 1} }] } };
