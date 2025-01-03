51d7cf97637815cc6a5be6d69cfde2f7ab3deff8
billykerz <34313952+billykerz@users.noreply.github.com>
2025-01-03 10:59:43 -0500

    Add FRESH_JOB_BOX entry

---
+++ stored-objects.js
@@ -0,0 +1,214 @@
+export const StoredObjects = {
+  FRESH_JOB_BOX: {
+    "id": "50435:30154",
+    "name": "H2401979",
+    "type": "FRAME",
+    "visible": true,
+    "locked": false,
+    "opacity": 1,
+    "x": -2417,
+    "y": -2351,
+    "width": 1800,
+    "height": 2911,
+    "rotation": 0,
+    "constrainProportions": false,
+    "constraints": {
+      "horizontal": "MIN",
+      "vertical": "MIN"
+    },
+    "fills": [
+      {
+        "type": "SOLID",
+        "visible": true,
+        "opacity": 1,
+        "blendMode": "NORMAL",
+        "color": {
+          "r": 1,
+          "g": 1,
+          "b": 1,
+          "a": 1
+        }
+      }
+    ],
+    "strokes": [],
+    "strokeWeight": 1,
+    "strokeAlign": "INSIDE",
+    "strokeCap": "NONE",
+    "strokeJoin": "MITER",
+    "dashPattern": [],
+    "cornerRadius": 0,
+    "cornerSmoothing": 0,
+    "effects": [],
+    "blendMode": "PASS_THROUGH",
+    "layoutProperties": {
+      "layoutMode": "VERTICAL",
+      "layoutWrap": "NO_WRAP",
+      "paddingLeft": 0,
+      "paddingRight": 0,
+      "paddingTop": 0,
+      "paddingBottom": 0,
+      "itemSpacing": 0,
+      "counterAxisSpacing": 0,
+      "primaryAxisAlignItems": "MIN",
+      "counterAxisAlignItems": "CENTER",
+      "primaryAxisSizingMode": "AUTO",
+      "counterAxisSizingMode": "AUTO"
+    },
+    "textProperties": null,
+    "componentProperties": null,
+    "starProperties": null,
+    "polygonProperties": null,
+    "vectorProperties": null,
+    "ellipseProperties": null,
+    "lineProperties": null,
+    "children": [
+      {
+        "id": "50435:30155",
+        "name": "Drob Box Header",
+        "type": "FRAME",
+        "visible": true,
+        "locked": false,
+        "opacity": 1,
+        "x": 0,
+        "y": 0,
+        "width": 1800,
+        "height": 265,
+        "rotation": 0,
+        "constrainProportions": false,
+        "constraints": {
+          "horizontal": "MIN",
+          "vertical": "MIN"
+        },
+        "fills": [
+          {
+            "type": "SOLID",
+            "visible": true,
+            "opacity": 1,
+            "blendMode": "NORMAL",
+            "color": {
+              "r": 0.2514811158180237,
+              "g": 0.2514811158180237,
+              "b": 0.2514811158180237,
+              "a": 1
+            }
+          }
+        ],
+        "strokes": [],
+        "strokeWeight": 1,
+        "strokeAlign": "INSIDE",
+        "strokeCap": "NONE",
+        "strokeJoin": "MITER",
+        "dashPattern": [],
+        "cornerRadius": 0,
+        "cornerSmoothing": 0,
+        "effects": [],
+        "blendMode": "PASS_THROUGH",
+        "layoutProperties": {
+          "layoutMode": "HORIZONTAL",
+          "layoutWrap": "NO_WRAP",
+          "paddingLeft": 113,
+          "paddingRight": 275,
+          "paddingTop": 75,
+          "paddingBottom": 75,
+          "itemSpacing": 10,
+          "counterAxisSpacing": 0,
+          "primaryAxisAlignItems": "MIN",
+          "counterAxisAlignItems": "CENTER",
+          "primaryAxisSizingMode": "FIXED",
+          "counterAxisSizingMode": "AUTO"
+        },
+        "textProperties": null,
+        "componentProperties": null,
+        "starProperties": null,
+        "polygonProperties": null,
+        "vectorProperties": null,
+        "ellipseProperties": null,
+        "lineProperties": null,
+        "children": [
+          {
+            "id": "50435:30156",
+            "name": "#JOBNAME",
+            "type": "TEXT",
+            "visible": true,
+            "locked": false,
+            "opacity": 1,
+            "x": 113,
+            "y": 103.5,
+            "width": 1412,
+            "height": 58,
+            "rotation": 0,
+            "constrainProportions": false,
+            "constraints": {
+              "horizontal": "MIN",
+              "vertical": "MIN"
+            },
+            "fills": [
+              {
+                "type": "SOLID",
+                "visible": true,
+                "opacity": 1,
+                "blendMode": "NORMAL",
+                "color": {
+                  "r": 1,
+                  "g": 1,
+                  "b": 1,
+                  "a": 1
+                }
+              }
+            ],
+            "strokes": [],
+            "strokeWeight": 1,
+            "strokeAlign": "OUTSIDE",
+            "strokeCap": "NONE",
+            "strokeJoin": "MITER",
+            "dashPattern": [],
+            "cornerRadius": null,
+            "cornerSmoothing": null,
+            "effects": [],
+            "blendMode": "PASS_THROUGH",
+            "layoutProperties": null,
+            "textProperties": {
+              "type": "TEXT",
+              "characters": "OCT1 FAB FALL PROMO FD RICH PUSH",
+              "fontName": {
+                "family": "Macys Sans",
+                "style": "Bold"
+              },
+              "fontSize": 48,
+              "letterSpacing": {
+                "unit": "PERCENT",
+                "value": -1
+              },
+              "lineHeight": {
+                "unit": "PERCENT",
+                "value": 120.00000476837158
+              },
+              "textAlignHorizontal": "LEFT",
+              "textAlignVertical": "CENTER",
+              "textAutoResize": "HEIGHT",
+              "textCase": "ORIGINAL",
+              "textDecoration": "NONE",
+              "textStyleId": "",
+              "paragraphIndent": 0,
+              "paragraphSpacing": 0,
+              "fills": [
+                {
+                  "type": "SOLID",
+                  "visible": true,
+                  "opacity": 1,
+                  "blendMode": "NORMAL",
+                  "color": {
+                    "r": 1,
+                    "g": 1,
+                    "b": 1,
+                    "a": 1
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
